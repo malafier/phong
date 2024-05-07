37e130471fe45033ab41f7a3ab41844846654d2b
Robert Małafiejew <rmalafiej@gmail.com>
2024-05-07 15:41:35 +0200

    small naming changes

--- lighting.py
+++ lighting.py
@@ -38,8 +38,7 @@ def diffuse_light(diffuse, normal, light_dir, colour):
 
 
 def specular_light(specular, normal, light_dir, view_dir, colour, shininess):
-    incident_vec = light_dir
-    reflected_vec = incident_vec - 2 * np.dot(incident_vec, normal) * normal
+    reflected_vec = light_dir - 2 * np.dot(light_dir, normal) * normal
     cos_angle = np.dot(view_dir, reflected_vec) / (np.linalg.norm(view_dir) * np.linalg.norm(reflected_vec))
     return specular * colour * np.power(cos_angle, shininess)
 
@@ -52,8 +51,8 @@ def phong_lighting(sphere, light, point, camera):
     vec_to_light = (light.position - point) / np.linalg.norm(light.position - point)
 
     ambient = ambient_light(light.ambient, sphere.colour)
-    diffuse = diffuse_light(light.diffuse, normal, -vec_to_light, sphere.colour)
-    specular = specular_light(light.specular, normal, -vec_to_light, vec_to_viewer, sphere.colour, sphere.shininess)
+    diffuse = diffuse_light(light.diffuse, normal, vec_to_light, sphere.colour)
+    specular = specular_light(light.specular, normal, vec_to_light, vec_to_viewer, sphere.colour, sphere.shininess)
     illumination = ambient + diffuse + specular
 
     illumination_list = [int(min(255, max(0, i))) for i in illumination]
